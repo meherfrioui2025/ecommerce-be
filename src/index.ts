a343e10cc80ff0a743c44c2e80a544f43510ea7b
Frioui <frioui@MacBook-Air-de-Frioui.local>
2025-06-12 21:31:49 +0200

    add ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express with TypeScript!");
